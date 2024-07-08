51cd32b7f10b454fb73d975b3f0d62ea05f51f76
IshaanVerma19 <ishaan4.mitmpl2023@learner.manipal.edu>
2024-07-09 03:56:08 +0530

    adding more files

---
+++ abc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class abc
+{
+    public static void main(String args[])
+    {
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int j=in.nextInt();
+        for(int i=0;i<j;i++)
+        {
+            System.out.println("Hello");
+        }        
+    }
+}
